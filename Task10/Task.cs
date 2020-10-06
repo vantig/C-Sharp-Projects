e0e33aed20123b4a5e8e6ca5c967b443e1ce0220
Vanya Gordeychik <vanya.gordejchik@mail.ru>
2020-10-06 14:23:57 +0300

    Prime number done

--- Task10/Task.cs
+++ Task10/Task.cs
@@ -1,8 +1,74 @@
 ﻿using System;
+using System.Linq;
 
 namespace Task10
 {
     public class Task
     {
+        public static int GetRandomNumber(int minNumber, int maxNumber)
+        {
+            Random random = new Random();
+            return random.Next(minNumber, maxNumber);
+
+        }
+
+        public static string PlayHeadsOrTails(int count)
+        {
+            int x1 = 0, x2 = 0;
+            for (int i = 0; i < count; i++)
+            {
+                if (GetRandomNumber(1, 2) == 1)
+                {
+                    x1++;
+                }
+                else
+                {
+                    x2++;
+                }
+            }
+
+            string str;
+            str = $"{x1} {x2}";
+            return str;
+        }
+
+
+        public static int FlipNumberWithoutZeros(int N)
+        {
+            int M = 0;
+            foreach (var letter in N.ToString().Reverse())
+            {
+                if (letter != '0')
+                {
+                    M *= 10;
+                    M += int.Parse(letter.ToString());
+                }
+            }
+            return M;
+
+        }
+        public static bool CheckInputNumberInRange(double number, double min)
+        {
+            return number >= min;
+        }
+
+        public static bool IsPrimeNumber(int number)
+        {
+            const int MIN_NUMBER = 2;
+            if (!CheckInputNumberInRange(number, MIN_NUMBER))
+            {
+                return false;
+            }
+
+            for (int i = 2; i < number; i++)
+            {
+                if (number%i==0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 }
--- UnitTestProject/UnitTest.cs
+++ UnitTestProject/UnitTest.cs
@@ -6,8 +6,45 @@ namespace UnitTestProject
     public class UnitTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void PlayHeadsOrTails_50()
         {
+            string temp = "", num1 = "", num2 = "";
+            var str = Task10.Task.PlayHeadsOrTails(50);
+            foreach (var VARIABLE in str)
+            {
+                if (VARIABLE != ' ')
+                {
+                    temp += VARIABLE;
+                }
+                else
+                {
+                    num1 = temp;
+                }
+            }
+
+
+
+            num2 = temp;
+            Assert.AreEqual(true, int.Parse(num1) > 0 && int.Parse(num2) > 0);
+        }
+
+        [TestMethod]
+        public void FlipNumberWithoutZeros_102_Returned_21()
+        {
+            var expected = 21;
+            Assert.AreEqual(expected,Task10.Task.FlipNumberWithoutZeros(102));
+        }
+        
+        [TestMethod]
+        public void IsPrimeNumber_5_Returned_true()
+        {
+            var expected = true;
+            Assert.AreEqual(expected, Task10.Task.IsPrimeNumber(5));
+        }
+        public void IsPrimeNumber_4_Returned_false()
+        {
+            var expected = false;
+            Assert.AreEqual(expected, Task10.Task.IsPrimeNumber(4));
         }
     }
 }
